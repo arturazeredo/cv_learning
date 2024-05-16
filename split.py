6d682e5db173f358507e976ae69a5e06c55d249a
arturazeredo <arturazeredo@gmail.com>
2024-05-16 16:22:35 -0300

    split code complete

---
+++ split.py
@@ -0,0 +1,23 @@
+# Importando bibliotecas
+import numpy as np
+import argparse
+import cv2
+# Argumentos
+ap= argparse.ArgumentParser()
+ap.add_argument("-i", "--image", required=True, help="Path to the image")
+args = vars(ap.parse_args())
+
+# Carregando imagem
+image = cv2.imread(args["image"])
+(B, G, R)= cv2.split(image) # Dividindo imagem em canais
+# Imagem Splitada
+cv2.imshow("Red", R)
+cv2.imshow("Green", G)
+cv2.imshow("Blue", B)
+cv2.waitKey(0)
+
+merged = cv2.merge([B, G, R])
+cv2.imshow("Merged", merged)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
